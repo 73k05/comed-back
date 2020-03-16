20d14e1b89d698413a562840ef3accadb5d985fb
axios-93 <fahdmajzoub13@gmail.com>
2020-03-16 10:19:16 +0100

    [front-gouv] enable - disable functionality is implemented to submit button

--- frontend/js/medical.js
+++ frontend/js/medical.js
@@ -1,18 +1,21 @@
 (function () {
     'use strict';
     window.addEventListener('load', function () {
-         populateSelect();
+        populateSelect();
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName('needs-validation');
         // Loop over them and prevent submission
         var validation = Array.prototype.filter.call(forms, function (form) {
             form.addEventListener('submit', function (event) {
                 if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
+                    console.log('not true');
+                    preventEvents(event);
                 }
                 else if (form.checkValidity() === true) {
-                    sendBookMail()
+                    console.log('form is true');
+                    preventEvents(event);
+                    disableBookButton(form);
+                    sendBookMail();
                 }
                 form.classList.add('was-validated');
             }, false);
@@ -59,7 +62,7 @@ function sendBookMail() {
 }
 
 function sendMail(form, checkbutton) {
-    if (form == undefined || !form.checkValidity() || form.is_already_sent.value == 'true') {
+    if (form == undefined || !form.checkValidity()) {
         console.error("Error, email empty or wrong or medical rendez-vous already sent");
         return;
     }
@@ -72,7 +75,6 @@ function sendMail(form, checkbutton) {
 
     // generate the contact number value
     var number = Math.random() * 100000 | 0;
-    var name = email.substring(0, email.indexOf('@'));
 
     var params = {
         email: email,
@@ -84,12 +86,29 @@ function sendMail(form, checkbutton) {
     };
 
     form.is_already_sent.value = 'true';
+    console.log('anything');
     emailjs.send('commissionmedicale', 'commissionmedicale_template', params)
-        .then(function(response) {
+        .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             checkbutton.removeClass("hide");
         }, function (error) {
+            enableBookButton(form);
             console.log('FAILED...', error);
             form.is_already_sent.value = false;
         });
 }
+
+function disableBookButton(form) {
+    form.bookButton.disabled = true;
+    return true;
+}
+
+function enableBookButton() {
+    form.bookButton.disabled = false;
+    return false;
+};
+
+function preventEvents(event){
+    event.preventDefault();
+    event.stopPropagation();
+};
