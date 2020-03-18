d9ddea8f6f53b07388029d8d7bd093998de68b5f
axios-93 <fahdmajzoub13@gmail.com>
2020-03-19 00:40:10 +0100

    [front-gouv] birth-name - progress-bar type-visit alert-messages E-mail params

--- frontend/js/medical.js
+++ frontend/js/medical.js
@@ -1,6 +1,5 @@
 const minimumDelayBook = 7;
 
-
 (function () {
     'use strict';
     window.addEventListener('load', function () {
@@ -11,14 +10,14 @@ const minimumDelayBook = 7;
         // Loop over them and prevent submission
         var validation = Array.prototype.filter.call(forms, function (form) {
             form.addEventListener('submit', function (event) {
+                submitBday();
                 if (form.checkValidity() === false) {
                     preventEvents(event);
                 }
                 else if (form.checkValidity() === true) {
-                    console.log('form is true');
                     preventEvents(event);
                     disableBookButton(form);
-                    sendBookMail();
+                     sendBookMail();
                 }
                 form.classList.add('was-validated');
             }, false);
@@ -41,17 +40,20 @@ function submitBday() {
     if (age < 18) {
         errorMessage.style.display = "block";
         birthdateInput.setCustomValidity('useless error message');
+        return false;
     }
 
     else if (age > 18) {
         birthdateInput.setCustomValidity('');
         errorMessage.style.display = "none";
+        return true;
     }
 
     else if (age === 18) {
+        return true;
         // your algorithm is our concern compute the exact day of the year 
     }
-}
+};
 
 // Email JS init
 (function () {
@@ -62,19 +64,23 @@ function submitBday() {
 function sendBookMail() {
     var form = document.getElementById('booking-form');
     sendMail(form, $("#contact-check"));
-}
+};
 
 function sendMail(form, checkbutton) {
     if (form == undefined || !form.checkValidity()) {
         console.error("Error, email empty or wrong or medical rendez-vous already sent");
         return;
-    }
+    };
 
     var email = form.email.value
     var firstname = form.firstname.value
     var lastname = form.lastname.value
     var phone = form.phone.value
     var birthdate = form.bday.value
+    var birthname = form.birthname.value
+    var region = form.selectregion.value
+    var selectcase = form.selectcase.value
+    var bookingdate = form.bookingdate.value
 
     // generate the contact number value
     var number = Math.random() * 100000 | 0;
@@ -85,27 +91,35 @@ function sendMail(form, checkbutton) {
         lastname: lastname,
         phone: phone,
         number: number,
-        birthdate: birthdate
+        birthdate: birthdate,
+        birthname: birthname,
+        region: region,
+        typevisit: selectcase,
+        datebooking: bookingdate 
     };
 
-    form.is_already_sent.value = 'true';
+    showProgressBar();
+
     emailjs.send('commissionmedicale', 'commissionmedicale_template', params)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
-            checkbutton.removeClass("hide");
+            hideProgressBar();
+            showSuccesMessage();
+
         }, function (error) {
             enableBookButton(form);
             console.log('FAILED...', error);
-            form.is_already_sent.value = false;
+            hideProgressBar();
+            showErrorMEssage();
         });
-}
+};
 
 function disableBookButton(form) {
     form.bookButton.disabled = true;
     return true;
-}
+};
 
-function enableBookButton() {
+function enableBookButton(form) {
     form.bookButton.disabled = false;
     return false;
 };
@@ -124,4 +138,25 @@ function initBookDatePicker() {
     nextWeekDate.setDate(nextWeekDate.getDate() + minimumDelayBook);
     displayBookingInput.min = nextWeekDate.getFullYear().toString() + '-' + (nextWeekDate.getMonth() + 1).toString().padStart(2, 0)
         + '-' + nextWeekDate.getDate().toString().padStart(2, 0);
-}
+};
+
+function showSuccesMessage() {
+    let succesMesage = document.getElementById("success-message");
+    setTimeout(function () { succesMesage.style.display = 'block'; }, 2000);
+};
+
+function showErrorMEssage() {
+    let errorMessage = document.getElementById("error-message");
+    setTimeout(function () { errorMessage.style.display = 'block'; }, 2000);
+
+};
+
+function showProgressBar() {
+    var progressBar = document.getElementById("progressId");
+    progressBar.hidden = false;
+};
+
+function hideProgressBar() {
+    var progressBar = document.getElementById("progressId");
+    setTimeout(function () { progressBar.hidden = true; }, 2000);
+};
