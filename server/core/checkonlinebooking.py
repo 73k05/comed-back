1822fbfb0bb9db694b1d9d6c479497e1454babf2
73k05 <olivier@inkos.fr>
2020-03-31 10:58:53 +0200

    [gouv][server] Get day zero

--- server/core/checkonlinebooking.py
+++ server/core/checkonlinebooking.py
@@ -1,73 +1,82 @@
+import datetime
 import json
-# importing the requests library 
+# importing the requests library
+import sys
+# Time lib to sleep
+import time
+
 import requests
 from requests.exceptions import HTTPError
-# Time lib to sleep
-import time	
-import datetime
 
-from mail import sendMail
+sys.path.insert(1, '../utils')
 from log import writeLog
+from dateutils import getmonthfromfrench
+from dateutils import getdatefromdata
 
 # Count number of request sent
-nbRequestSent=0
+nbRequestSent = 0
 urlDepartmentList = {}
 bookingOngoingList = {}
 
 # Load dep list
-with open('./json/gouvendpoints.json') as json_data:
+with open('../json/gouvendpoints.json') as json_data:
     urlDepartmentList = json.load(json_data)["gouvUrlList"]
 
 # Load Booking Ongoing List
-with open('../frontend/resources/bookingongoing.json') as json_data:
-    urlDepartmentList = json.load(json_data)["gouvUrlList"]
+with open('../../frontend/resources/bookingongoing.json') as json_data:
+    bookingList = json.load(json_data)["bookings"]
 
 # fake header to bypass security
-headers = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
+headers = {
+    "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
 
-while 1==1:
-	
-	nbRequestSent+=1
+while 1 == 1:
 
-	# Check all prefs
-	for departmentUrl in urlDepartmentList:
+    nbRequestSent += 1
 
-		now = datetime.datetime.now()
-		writeLog("["+now.strftime("%H:%M") + "]Department: "+departmentUrl)
-		
-		# Send request to gouv
-		# sending get request and saving the response as response object
-		try:
-			response = requests.post(departmentUrl, headers = headers, data={"condition":"on","nextButton":'Effectuer une demande de rendez-vous'})
-			# If the response was successful, no Exception will be raised
-			response.raise_for_status()
-		except HTTPError as http_err:
-			writeLog(f"\r\nRequest HTTP error occurred: {http_err}\r\n")  # Python 3.6
-			break
-		except Exception as err:
-			writeLog(f"\r\nRequest Other error occurred: {err}\r\n")  # Python 3.6
-			break
-		else:
-			# extracting data in raw text format
-			# data = response.content
-			data = response.text
-			writeLog(data)
-			
-			indexFooter = data.find('<footer>') 
-			writeLog(f"\r\nIndex footer: {indexFooter} \r\n")
+    # Check all prefs
+    for booking in bookingList:
+        now = datetime.datetime.now()
+        code = booking["department"]
+        urlEndPoint = urlDepartmentList[code - 1]["url"]
+        print(urlDepartmentList[code - 1])
+        writeLog("[" + now.strftime("%H:%M") + "]Booking of: " + booking[
+            "email"] + " code: " + str(code) + " request: " + urlEndPoint)
+        # Send request to gouv
+        # sending get request and saving the response as response object
+        try:
+            response = requests.get(urlEndPoint, headers=headers)
+            # If the response was successful, no Exception will be raised
+            response.raise_for_status()
+        except HTTPError as http_err:
+            writeLog(f"\r\nRequest HTTP error occurred: {http_err}\r\n")  # Python 3.6
+            break
+        except Exception as err:
+            writeLog(f"\r\nRequest Other error occurred: {err}\r\n")  # Python 3.6
+            break
+        else:
+            # extracting data in raw text format
+            # data = response.content
+            data = response.text
 
-			found = data.find('ultérieurement',0,(indexFooter)) != -1
-			writeLog(f"Found: {found} \r\n")
-			# Send email when slot found
-			if found:
-				writeLog(": [CLOSED]\r\n")
-			# Send email when slot found
-			else:
-				writeLog(": [OPEN]\r\n")
+            dateZero = getdatefromdata(data)
+            print(dateZero.strftime("%d/%m/%y"))
 
-	# Sleeping time in minutes
-	sleeptime = 1
-	
-	writeLog("============ 73kBot will sleep "+str(sleeptime)+" minutes _o/ "+str(nbRequestSent)+" ============"+ "\r\n")
-	
-	time.sleep(sleeptime*60)
+            found = data.find('plage libre', 0) != -1
+            writeLog(f"Found: {found} \r\n")
+            # Send email when slot found
+            if found:
+                writeLog(": [OPEN]\r\n")
+                print("Bingo!")
+            # Send email when slot found
+            else:
+                writeLog(": [CLOSED]\r\n")
+                print("Close -_-")
+
+    # Sleeping time in minutes
+    sleeptime = 1
+
+    writeLog("============ 73kBot will sleep " + str(sleeptime) + " minutes _o/ " + str(
+        nbRequestSent) + " ============" + "\r\n")
+
+    time.sleep(sleeptime * 60)
