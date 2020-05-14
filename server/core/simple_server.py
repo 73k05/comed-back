8e054276c0bff724e9ea263b253c05dc648a8b0e
73k05 <olivier@inkos.fr>
2020-05-14 11:46:41 +0200

    [gouv][server] Change logs and CORS

--- server/core/simple_server.py
+++ server/core/simple_server.py
@@ -12,7 +12,7 @@ class EnableCors(object):
     def apply(fn, context):
         def _enable_cors(*args, **kwargs):
             # set CORS headers
-            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Origin'] = 'https://www.commissionmedicale.fr'
             response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
             response.headers[
                 'Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
