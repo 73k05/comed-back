0931bdcfdf8e02f7624355df9f94d0b84227e19d
73k05 <olivier@inkos.fr>
2020-05-14 14:00:26 +0200

    [gouv][server] Handle error

--- server/core/restart.py
+++ server/core/restart.py
@@ -3,22 +3,24 @@ from subprocess import Popen
 import psutil
 
 for process in psutil.process_iter():
-    if process.cmdline() == ['python3.7', 'checkonlinebooking.py', '&']:
-        print('Process found kill checkonlinebooking.py Terminating it. -Sarah Conor-')
-        try:
+
+    try:
+        if process.cmdline() == ['python3.7', 'checkonlinebooking.py', '&']:
+            print('Process found kill checkonlinebooking.py Terminating it. -Sarah Conor-')
             process.terminate()
-        except ProcessLookupError as err:
-            print(f'Error while terminating{err}')
-        except FileNotFoundError as err:
-            print(f'Error while terminating{err}')
-    if process.cmdline() == ['python3.7', 'simple_server.py', '&']:
-        print('Process found kill simple_server.py Terminating it. -Sarah Conor-')
-        try:
+    except ProcessLookupError as err:
+        print(f'Error while terminating{err}')
+    except FileNotFoundError as err:
+        print(f'Error while terminating{err}')
+
+    try:
+        if process.cmdline() == ['python3.7', 'simple_server.py', '&']:
+            print('Process found kill simple_server.py Terminating it. -Sarah Conor-')
             process.terminate()
-        except ProcessLookupError as err:
-            print(f'Error while terminating{err}')
-        except FileNotFoundError as err:
-            print(f'Error while terminating{err}')
+    except ProcessLookupError as err:
+        print(f'Error while terminating{err}')
+    except FileNotFoundError as err:
+        print(f'Error while terminating{err}')
 
 git_pull_process = Popen(["git", "pull"])
 git_pull_process.wait()
