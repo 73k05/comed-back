e13497c9f96cbca087c7058e4ae1be8384ad46b8
73k05 <olivier@inkos.fr>
2020-05-15 15:20:58 +0200

    [gouv][server] README.md & encoding

--- README.md
+++ README.md
@@ -2,21 +2,18 @@
 check if a slot is libre in for visite medical http://www.rdv.puy-de-dome.gouv.fr/booking/create/801
 
 
-# install Python & python-utils
-install python 3.7
+# Python 3.7 & python-utils
 ```
 sudo apt-get update -y
-```
 sudo apt-get install -y python3-python-utils
 ```
 ## **Note:** 
 after running the server if you faced error: unsupported locale setting then you need to install FR language on your device system
 then run the following commands:
 
+```
 export LC_ALL="fr_FR.UTF-8"
-```
 export LC_CTYPE="fr_FR.UTF-8" 
-```
 sudo dpkg-reconfigure locales
 ```
 
@@ -25,6 +22,7 @@ sudo dpkg-reconfigure locales
 ```
 python3.7 checkonlinebooking.py
 ```
+
 ## Or if you want to start and quit
 ```
 nohup python3.7 checkonlinebooking.py &
--- server/utils/jsonutils.py
+++ server/utils/jsonutils.py
@@ -8,14 +8,14 @@ def write_department_availability(departments):
     copyfile(dep_availability_json, '../../frontend/resources/json/departmentavailabilities_bk.json')
     with open(dep_availability_json, "w+") as json_file:
         json_data = {"departments": departments}
-        json.dump(json_data, json_file)
+        json.dump(json_data, json_file, ensure_ascii=False, indent=2)
 
 # Write in json file
 def write_ongoing_booking(bookings):
     bookings_json = '../../frontend/resources/json/bookingongoing.json'
     with open(bookings_json, "w", encoding='utf-8') as json_file:
         json_data = {"bookings": bookings}
-        json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+        json.dump(json_data, json_file, ensure_ascii=False, indent=2)
 
 
 def add_department_to_list(department_code, department_name, booking, department_book_url,
